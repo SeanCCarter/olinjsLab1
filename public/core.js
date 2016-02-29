d9fbbfd17a2d8379b0469e71adb238aa70b12ccc
SeanCCarter <seancarter26@gmail.com>
2016-02-28 20:20:23 -0500

    Added editing and creation forms, and selectors to show them.

--- public/core.js
+++ public/core.js
@@ -1,1 +1,36 @@
 // Angular stuff goes here
+angular.module('CardWiki',[
+
+])
+.controller("mainController", function($scope) {
+
+	$scope.currentArticle = null;
+	$scope.editing = false;
+	$scope.creating = false;
+	$.get("/articles", {}) //TODO: Update route name
+		//Gets all the articles from the database
+		//Won't work if they get too bit, but neither would
+		//our ideas for displaying it
+		.done(function(data, status){
+			$scope.articles = data;
+			$scope.$apply(); //Refresh the side collumn
+		})
+		.error(function(err, status){
+			console.log(err)
+			console.log(status)
+		});
+
+	$scope.shouldShowHomepage = function(){
+		if ($scope.currentArticle===null){return true}
+	}
+
+	$scope.shouldShowArticle = function(){
+		if ($scope.currentArticle!==null){return true}
+	}
+	$scope.shouldShowEditing = function(){
+		if ($scope.editing===true){return true}
+	}
+	$scope.shouldShowCreating = function(){
+		if ($scope.creating===true){return true}
+	}
+})
